347cb5f4fb4b88527c75b15e50ac3132fb8a0c53
André Lefon <110571264+AndreLefon@users.noreply.github.com>
2022-10-03 22:23:28 -0300

    k

---
+++ André/Lista1/ex9.py
@@ -0,0 +1,10 @@
+##https://www.beecrowd.com.br/judge/pt/problems/view/1008
+
+numfunc = int(input())
+hrtrab = int(input())
+valorhr = float(input())
+
+salario = float(hrtrab * valorhr)
+
+print("NUMBER = %d" %numfunc)
+print("SALARY = U$ %0.2f" %salario)
